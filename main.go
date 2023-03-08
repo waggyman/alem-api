4317c9a06db42ad1aaa3afad28a6f4e9ed3e906d
Arigi Wiratama <arigi@ofload.com>
2023-03-08 23:02:54 +0700

    Fix method name

--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
+
 import (
-    // "net/http"
-    "github.com/gin-gonic/gin"
+	// "net/http"
+	"github.com/gin-gonic/gin"
 	"github.com/waggyman/alem-api/controllers"
 	"github.com/waggyman/alem-api/models"
 )
@@ -12,8 +13,9 @@ func main() {
 	// teachers side
 	router.GET("/teachers", controllers.GetTeachers)
 	router.POST("/teachers", controllers.StoreTeacher)
-	router.GET("/teachers/:id", controllers.GetTeacherByID)
-	router.DELETE("/teachers/:id", controllers.DeleteTeacherByID)
+	router.GET("/teachers/:id", controllers.FindTeacher)
+	router.PATCH("/teachers/:id", controllers.UpdateTeacher)
+	router.DELETE("/teachers/:id", controllers.RemoveTeacher)
 
 	router.Run("localhost:8000")
 }
